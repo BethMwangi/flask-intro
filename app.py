34b86b8281c6ffca068450031674b5c83d9fe65b
BethMwangi <wanjikumwangib@gmail.com>
2016-05-12 03:54:30 +0300

    added gitignore file and login methods

--- app.py
+++ app.py
@@ -9,6 +9,17 @@ def home():
 def welcome():
    return render_template("welcome.html")
 
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid credentials.Please try again.'
+        else:
+            return redirect(url_for('home'))
+
+    return render_template('login.html, error=error')
+
 
 if __name__== '__main__':
     app.run(debug=True)
