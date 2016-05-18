7fb6b385ad84973db38a8e88cac9841fd32eb4b0
BethMwangi <wanjikumwangib@gmail.com>
2016-05-19 01:17:36 +0300

    created connections to databases

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, redirect, url_for, request, session, flash
 
 from functools import wraps
+import sqlite3
 
 
 
@@ -8,6 +9,8 @@ from functools import wraps
 app = Flask(__name__)
 
 app.secret_key = "my precious"
+app.database = "sample.db"
+
 
 #login required decorator
 
@@ -26,14 +29,14 @@ def login_required(f):
 @login_required
 def home():
     return render_template('index.html')
-#    g.db = connect.db()
-#    cur = g.db.execute('select  * from posts')
-#    posts = [dict(title = row[0], description = row[1]) for row in cur.fetchall()]
-#    g.db.close()
+    g.db = connect.db()
+    cur = g.db.execute('select  * from posts')
+    posts = [dict(title = row[0], description = row[1]) for row in cur.fetchall()]
+    g.db.close()
 
-#    return render_template('index.html', posts = posts) #render a template
+    return render_template('index.html', posts = posts) #render a template
 
-     #return "Hello, world!"
+     return "Hello, world!"
 
 
 
@@ -60,8 +63,8 @@ def logout():
     flash('You were just logged out!')
     return redirect(url_for('welcome'))
 
-#def connect_db():
-#    return sqlite3.connect(app.database)
+def connect_db():
+    return sqlite3.connect(app.database)
 
 if __name__== '__main__':
     app.run(debug=True)
