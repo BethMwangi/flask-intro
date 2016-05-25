f476fa7b4b880f903d938fce82b1d51fadd92de9
BethMwangi <wanjikumwangib@gmail.com>
2016-05-26 00:34:40 +0300

    login page test case passed

--- test.py
+++ test.py
@@ -3,10 +3,18 @@ import unittest
 
 
 class FlaskTextCase(unittest.TestCase):
-    def text_index(self):
+    #to check whether flask was set up correctly
+    def test_index(self):
         tester= app.test_client(self)
         response = tester.get('/login', content_type='html/text')
         self.assertEqual(response.status_code, 200)
+    
+    #ensure login page was set up correctly
+    def test_login_page_loads(self):
+        tester = app.test_client(self)
+        response = tester.get('/login', content_type='html/text')
+        self.assertTrue('Please login' in response.data)
+        
         
 if __name__ =='__main__':
     unittest.main()
