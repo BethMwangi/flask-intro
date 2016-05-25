8c5f6ffad40cabfad0279f5a132c6d78b43cd33f
BethMwangi <wanjikumwangib@gmail.com>
2016-05-26 01:00:34 +0300

    logged in page test case passed

--- test.py
+++ test.py
@@ -14,7 +14,19 @@ class FlaskTextCase(unittest.TestCase):
         tester = app.test_client(self)
         response = tester.get('/login', content_type='html/text')
         self.assertTrue('Please login' in response.data)
-        
+    
+    #ensure login page behaves correctly when given correct credentials
+    def test_correct_login(self):
+        tester = app.test_client(self)
+        response = tester.post(
+            '/login', 
+            data=dict(username="admin", password="admin"), 
+            follow_redirects=True
+        )
+        self.assertIn('You were just logged in!', response.data)
+  
+
+    #ensure logout behaves correctly 
         
 if __name__ =='__main__':
     unittest.main()
